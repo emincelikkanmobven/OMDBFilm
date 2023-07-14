879be3ec5343b59b12c7f69c2c867b9f9d3a5851
emincelikkanmobven <emin.celikkan@mobven.com>
2023-07-14 17:57:29 +0300

    Initial Commit

---
+++ OMDBFilm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OMDBFilm
+//
+//  Created by Emin Çelikkan on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
