0444649e7f0ffda37e5f3849e7a22ef96fda6f6a
emincelikkanmobven <emin.celikkan@mobven.com>
2023-07-15 19:12:29 +0300

    FilmViewModel struct created with the values that will be used.

--- OMDBFilm/HomePage/HomePageModels.swift
+++ OMDBFilm/HomePage/HomePageModels.swift
@@ -9,22 +9,34 @@ import Foundation
 
 // swiftlint:disable nesting
 enum HomePage {
-    
-    enum Case {
-        
+
         struct Request {
             
         }
         
         struct Response {
-            
+            var response: FilmResponse?
         }
         
         struct ViewModel {
-            
+             struct FilmViewModel {
+                var title: String?
+                var year: String?
+                var rated: String?
+                var released: String?
+                var genre: String?
+                var director: String?
+                var ratings: [Rating]?
+                var type: String?
+                var poster: String?
+                var imdbRating: String?
+                var metaScore: String?
+                var plot: String?
+                var revenue: String?
+            }
         }
         
-    }
+    
     
 }
 // swiftlint:enable nesting
