7369e347db5aebfaa7849aa7e950ba02f7b43d39
emincelikkanmobven <emin.celikkan@mobven.com>
2023-07-16 21:23:14 +0300

    presenter funcs created to be able to present the data on viewController's display functions.

--- OMDBFilm/Scenes/FilmDetail/FilmDetailPresenter.swift
+++ OMDBFilm/Scenes/FilmDetail/FilmDetailPresenter.swift
@@ -6,13 +6,27 @@
 //
 
 import Foundation
+import UIKit
 
 protocol FilmDetailPresentationLogic: AnyObject {
-    
+    func presentFilmDetails(viewModel: FilmDetail.ViewModel)
+    func presentFilmDetailImage(_ withImage: UIImage)
+    func presentErrorWithMessage(_ message: String)
 }
 
 final class FilmDetailPresenter: FilmDetailPresentationLogic {
     
     weak var viewController: FilmDetailDisplayLogic?
     
+    func presentErrorWithMessage(_ message: String) {
+        viewController?.displayErrorWithMessage(message)
+    }
+    
+    func presentFilmDetailImage(_ withImage: UIImage) {
+        viewController?.displayFilmImage(withImage)
+    }
+    
+    func presentFilmDetails(viewModel: FilmDetail.ViewModel) {
+        viewController?.displayFilmDetails(viewModel: viewModel)
+    }
 }
